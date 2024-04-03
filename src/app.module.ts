75126939d586b6f236c4134f94f403a52b921c1e
MrMoon <abuamaramfreelance@gmail.com>
2024-04-03 14:33:07 +0300

    Add asset service and controller structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { CoursesModule } from './courses/courses.module';
 import { GroupsModule } from './groups/groups.module';
 import { UserModule } from './user/user.module';
+import { AssetModule } from './asset/asset.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule, GroupsModule, UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule, GroupsModule, UserModule, AssetModule],
   controllers: [AppController],
   providers: [AppService],
 })
