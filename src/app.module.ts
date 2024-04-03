49ffc5409e3612f1231941a24cf8aaca4166f5d6
MrMoon <abuamaramfreelance@gmail.com>
2024-04-03 14:30:57 +0300

    Add cources service and controller structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
