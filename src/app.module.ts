3b13b4bde769a9eadf84b7ea973954c14a0bcdeb
MrMoon <abuamaramfreelance@gmail.com>
2024-04-03 14:32:01 +0300

    Add groups service and controller structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { CoursesModule } from './courses/courses.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule, GroupsModule],
   controllers: [AppController],
   providers: [AppService],
 })
