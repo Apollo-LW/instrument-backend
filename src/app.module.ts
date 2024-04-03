9baa31bad4abfff5e74f5f0b2c624eb9794bbe0b
MrMoon <abuamaramfreelance@gmail.com>
2024-04-03 14:32:28 +0300

    Add user service and controller structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 import { CoursesModule } from './courses/courses.module';
 import { GroupsModule } from './groups/groups.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule, GroupsModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), NotesModule, TasksModule, CoursesModule, GroupsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
