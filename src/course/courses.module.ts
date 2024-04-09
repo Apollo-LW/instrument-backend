24e0946c1bf84d8f47aa2ea5ed0b3fa4d4ceab98
MrMoon <abuamaramfreelance@gmail.com>
2024-04-09 16:54:46 +0300

    Add asset course join

--- src/course/courses.module.ts
+++ src/course/courses.module.ts
@@ -7,6 +7,7 @@ import { CourseUser, CourseUserSchema } from './schema/courseuser.schema';
 import { CourseTask, CourseTaskSchema } from './schema/coursetask.schema';
 import { CourseNote, CourseNoteSchema } from './schema/coursenote.schema';
 import { CourseGraph, CourseGraphSchema } from './schema/coursegraph.schema';
+import { CourseAsset, CourseAssetSchema } from './schema/courseasset.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([
@@ -15,6 +16,7 @@ import { CourseGraph, CourseGraphSchema } from './schema/coursegraph.schema';
     { name: CourseTask.name, schema: CourseTaskSchema },
     { name: CourseNote.name, schema: CourseNoteSchema },
     { name: CourseGraph.name, schema: CourseGraphSchema },
+    { name: CourseAsset.name, schema: CourseAssetSchema },
   ])],
   controllers: [CoursesController],
   providers: [CourseService],
